8b76b4dca7c4a441a3e2e22730f68fa92f628e6d
wiliami <wiliaup97@gmail.com>
2024-06-12 04:13:41 -0400

    add config end

--- src/http/server.js
+++ src/http/server.js
@@ -1,11 +1,14 @@
 import { App } from "./app.js";
 
-const app = new App();
+const startServer = async () => {
+    const app = new App();
 
-app.listen(3333)
-    .then(message => {
+    try {
+        const message = await app.listen(3333);
         console.log(message);
-    })
-    .catch(err => {
-        console.error('Failed to start server:', err);
-    });
+    } catch (error) {
+        console.log('Failed to start server:', err);
+    }
+}
+
+startServer();
