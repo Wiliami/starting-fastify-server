6d647fd77775cd15ad7ee11fd53ae4ec0c264980
wiliami <wiliaup97@gmail.com>
2024-06-17 03:40:21 -0400

    feat: add render html page

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,8 +1,23 @@
 async function homeRoutes(fastify, options) {
     fastify.get('/home', (req, reply) => {
-        const users = ['Wiliamis', 'Teste', 'Kipper dev', 'Dev Mario Soutinho'];
+        const users = [
+            { id: 1, name: 'Wiliamis' },
+            { id: 2, name: 'Kipper dev' },
+            { id: 3, name: 'Dev Mario Soutinho' },
+            { id: 4, name: 'Lucas Montano' }
+        ]
 
-        reply.view('index', users);
+        const buttonActions = {
+            button: function() {
+                console.log('Botão clicado');
+            }
+        }
+          
+        reply.view('index', { 
+            users,
+            username: 'Wiliamis',
+            buttonActions
+        });
     });
 };
 
