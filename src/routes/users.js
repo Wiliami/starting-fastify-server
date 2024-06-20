3bcf03f3a322ae26ac403f6ba98256958332e9ea
wiliami <wiliaup97@gmail.com>
2024-06-19 23:26:33 -0400

    feat: new feature

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,15 +1,20 @@
-async function homeRoutes(fastify, options) {
+async function usersRoute(fastify, options) {
     fastify.get('/users', (req, reply) => {
         const users = [
             { id: 2, name: 'Kipper dev' },
             { id: 3, name: 'Dev Mario Soutinho' },
             { id: 4, name: 'Lucas Montano' }
         ];
+
+        const user = req.body;
           
         const tags = ['js', 'javascript'];
-        
-        reply.view('users', { users });
+
+        reply.view('users', { 
+            users,
+            tags
+        });
     });
 };
 
-export default homeRoutes;
+export default usersRoute;
