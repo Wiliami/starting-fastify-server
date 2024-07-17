eac99dea019850805071dc2e2a7565080d060c61
wiliami <wiliaup97@gmail.com>
2024-07-17 18:02:32 -0400

    feat: new feature

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,11 @@ import home from './home.js';
 import users from './users.js';
 import tags from './tags.js';
 import products from './products.js';
-import movies from './releases/movies.js';
+import login from './login.js';
+import register from './register.js';
 
 async function Routes(fastify, options) {
-    const routes = [ home, users, tags, products, movies ];
+    const routes = [ home, users, tags, products, register, login ];
     
     routes.forEach(route => fastify.register(route));
 }
