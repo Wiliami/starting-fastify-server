3467ee8f6a40750b62c9c11742d2562e68dc3d98
wiliami <wiliaup97@gmail.com>
2024-07-18 17:18:23 -0400

    feat: new feature

--- src/routes/releases/movies.js
+++
@@ -1,9 +0,0 @@
-import { movies } from '../../db.js';
-
-async function Movies(fastify, options) {
-    fastify.get('/movies', (req, reply) => {
-        reply.view('releases/movies', { movies });
-    })
-}
-
-export default Movies;
